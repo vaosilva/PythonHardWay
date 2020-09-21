73cb51b16aa282935db6ec5e3ccca86aaf2c17f0
vaosilva <svitor95@hotmail.com>
2020-09-21 23:41:18 +0100

    Exercise 1. A Good First Program (and renamed file from exercise 0)

---
+++ ex1/ex1.py
@@ -0,0 +1,15 @@
+# MAIN EXERCISE
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# STUDY DRILLS
+# 1. make your script print another line.
+print("Wow! Such line.")
+# 2. make your script print only one of the lines
+# a possible solution is per example commenting all the lines above of the main exercise
+# 3. exercise to test the effect of comments, already knew, even suggested it to solve the problem of study drill #2
