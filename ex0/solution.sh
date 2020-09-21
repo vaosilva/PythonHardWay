4f2d22c077ac2b746cdc3f4c81cfad407328b771
vaosilva <svitor95@hotmail.com>
2020-09-21 23:22:50 +0100

    Exercise 0. The Setup

---
+++ ex0/solution.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#1. install text editor, i'm using VSCode
+#2. get the text editor easily accessible, no problem with VSCode it was already configured in Windows PATH
+#3. run PowerShell, i'm using Git Bash because I'm more used to linux commands
+#4. make a shortcut for the terminal, no problem again as i have the Windows Terminal pinned to taskbar with Git Bash configured
+#5. open the terminal
+#6. install python (already had)
+#7. verify PATH variable of python (already confirmed)
+#8. exit python by entering quit() in the python interactive prompt
+#9. confirm you are back in the Git Bash, yes, i had success
+#10. to 14. play around in the directories and create a file with VSCode
+mkdir ex0
+cd ex0
+code solution.sh #which is this file :)
+#edit, save and confirm file is in directory
+ls
