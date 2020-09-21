cf8b52a782533014d7e663362800216156146b15
vaosilva <svitor95@hotmail.com>
2020-09-22 00:14:57 +0100

    Exercise 2. Comments and Pound Characters

---
+++ ex2/ex2.py
@@ -0,0 +1,17 @@
+# MAIN EXERCISE
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this")  # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+# STUDY DRILLS
+# 1. did i knew about the # character? 
+# yes, it comments lines and its name is octothorpe or pound character
+# 2. to 4. basically error checking, i had no problem with it; 
+# it was suggested to read code backward cause it's a trick to make your brain 
+# not attach meaning to each part of the code and rethink the problem
